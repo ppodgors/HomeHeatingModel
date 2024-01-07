f3421cf9d6f4fb45b41325ab906657991b5b0a27
Paulina Podgórska <paulinapodgorska98@gmail.com>
2024-01-07 23:25:04 +0100

    Modifying the class house, visualising the house diagram.

--- house-class.py
+++ house-class.py
@@ -1,17 +1,31 @@
 import numpy as np 
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 class house:
     def __init__(self,params):
         self.matrix= {}
+        self.home = np.zeros((101,101))
         for key in params["indexes"].keys():
-            ind_min_x = min(key[:,0])
-            ind_max_x = max(key[:,0])
-            ind_min_y = min(key[:,1])
-            ind_max_y = max(key[:,1])
+            room = params["indexes"][key]
+            ind_min_x = min(room[:,0])
+            ind_max_x = max(room[:,0])
+            ind_min_y = min(room[:,1])
+            ind_max_y = max(room[:,1])
             self.matrix[key] = np.zeros(((ind_max_x-ind_min_x,ind_max_y-ind_min_y)))
+            self.home[ind_min_x:ind_max_x,ind_min_y:ind_max_y] = 1
+            self.home[ind_min_x,ind_min_y:ind_max_y]=2
+            self.home[ind_max_x,ind_min_y:ind_max_y]=2
+            self.home[ind_min_x:ind_max_x,ind_min_y]=2
+            self.home[ind_min_x:ind_max_x,ind_max_y]=2
+    def show(self):
+        cmapmine = ListedColormap(['w','b','black'], N=3)
+        plt.imshow(self.home,cmap=cmapmine)
+        plt.show()
+        print(self.home[0,:])
+
 
 if __name__=="__main__":
-  params = {"indeksy":{"`I": np.array(((0,0),(0,35),(20,35),(20,0))),"II" : np.array(((20,0),(20,35),(100,35),(100,0)))}}
-
-
-  home = house(params)
+    params = {"indexes":{"`I": np.array(((0,0),(0,35),(40,0),(40,35))),"II" : np.array(((40,0),(40,35),(100,0),(100,35))), "III":np.array(((40,35),(40,60),(100,35),(100,60))),"IV":np.array(((40,60),(40,100),(80,60),(80,100))),"V":np.array(((80,60),(80,100),(100,60),(100,100))) }}
+    home = house(params)
+    home.show()
